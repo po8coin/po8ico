4e0d073adbc35abadecfc22d005582bd1e9683f1
Rolf Bansbach <rolfbans@gmail.com>
2018-03-28 21:33:00 -0400

    manually set migration parameters

--- migrations/3_private_sale.js
+++ migrations/3_private_sale.js
@@ -1,26 +1,24 @@
 var PrivateSale = artifacts.require("./PO8/crowdsale/PrivateSale.sol");
 var meta = require('../migrations/meta/meta');
-var latestTime = require('../test/helpers/latestTime');
-var ether = require('../test/helpers/ether');
-var duration = require('../test/helpers/increaseTime').duration;
+var Web3 = require('web3');
 
 module.exports = function(deployer, network, accounts) {
 
-    console.log(deployer);
+
     /*PO8 Private Sale*/
-    console.log(latestTime.default());
-    var openTime =  latestTime.default() + 3;//the private sale will start in 3 seconds after deployed
+
+    var openTime =  Date.now() + 3;//the private sale will start in 3 seconds after deployed
     var config = {
         token: {
             address: meta.po8Token.address
         },
         privateSale: {
             open: openTime,
-            close: openTime + (duration.seconds(30)), //will end 30 sec later
-            cap: ether(300), //max ether allowed to receive
-            rate: new web3.BigNumber(5), // ether to token ratio
-            bonusRate: new web3.BigNumber(0.5), //bonus rate percentage 50%
-            minWei: ether(1), //minimum investment
+            close: openTime + 30, //will end 30 sec later
+            cap: '500000000000000000000', //max ether allowed to receive
+            rate: 5, // ether to token ratio
+            bonusRate: 0.5, //bonus rate percentage 50%
+            minWei: '1000000000000000000', //minimum investment
             walletEth: accounts[0],
             token: meta.po8Token.address,
             walletToken: accounts[0],
@@ -45,6 +43,6 @@ module.exports = function(deployer, network, accounts) {
         config.privateSale.walletToken,
         config.privateSale.tx_options
     ).then(function(){
-        console.log(PrivateSale);
+
     });
 };
