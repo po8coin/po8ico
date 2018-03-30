a1cae05d7512f6265b6b24bef7fa1615e793c90f
Rolf Bansbach <rolfbans@gmail.com>
2018-03-29 21:19:21 -0400

    read migrations config from json file and add it to git ignore

--- migrations/3_private_sale.js
+++ migrations/3_private_sale.js
@@ -1,48 +1,34 @@
-var PrivateSale = artifacts.require("./PO8/crowdsale/PrivateSale.sol");
+var PrivateSaleContract = artifacts.require("./PO8/crowdsale/PrivateSale.sol");
 var meta = require('../migrations/meta/meta');
+var configFile = require('../migrations/config/config');
 
 module.exports = function(deployer, network, accounts) {
 
 
     /*PO8 Private Sale*/
-
-    var openTime =  Date.now() + 3;//the private sale will start in 3 seconds after deployed
-    var config = {
-        token: {
-            address: meta.po8Token.address
-        },
-        privateSale: {
-            open: openTime,
-            close: openTime + 30, //will end 30 sec later
-            cap: '500000000000000000000', //max ether allowed to receive 500
-            rate: 5, // ether to token ratio
-            bonusRate: 0.5, //bonus rate percentage 50%
-            minWei: '1000000000000000000', //minimum investment 1 ether
-            walletEth: accounts[0],
-            token: meta.po8Token.address,
-            walletToken: accounts[0],
-            tx_options: {
-                from: accounts[0],
-                gas: 500000000
-            }
-        }
+    let config = configFile.privateSale.config;
+    //the private sale will start in 3 seconds after deployed
+    config.open = Date.now() + 3;
+    config.token = {
+        address: meta.po8Token.address
     };
 
 
+
     deployer.deploy(
-        PrivateSale,
-        config.privateSale.open,
-        config.privateSale.close,
-        config.privateSale.cap,
-        config.privateSale.rate,
-        config.privateSale.bonusRate,
-        config.privateSale.minWei,
-        config.privateSale.walletEth,
-        config.privateSale.token,
-        config.privateSale.walletToken,
-        config.privateSale.tx_options
+        PrivateSaleContract,
+        config.open,
+        config.close,
+        config.cap,
+        config.rate,
+        config.bonusRate,
+        config.minWei,
+        config.walletEth,
+        config.token.address,
+        config.walletToken,
+        config.tx_options
     ).then(function(){
-        //set up token allowance
+        //post deployment set up
 
     });
 };
